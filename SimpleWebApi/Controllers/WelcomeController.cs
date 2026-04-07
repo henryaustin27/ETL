8395520ce017659cad6a8bb5eb3bc24fc2bea0a9
henry austin <henrynewyork@gmail.com>
2026-04-07 11:22:37 -0400

    Created SimpleWebApi with welcome endpoint

---
+++ SimpleWebApi/Controllers/WelcomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WelcomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Welcome to SimpleWebApi" });
+        }
+    }
+}
